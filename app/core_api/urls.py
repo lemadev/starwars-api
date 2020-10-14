cc0fd60060f5d8abd546edae842570bcaed7c924
Franco Lema <francoe.lema.dev@gmail.com>
2020-10-14 18:01:11 -0500

    Añadido get para char

--- app/core_api/urls.py
+++ app/core_api/urls.py
@@ -2,10 +2,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from core_api.views import charTest
+from core_api.views import charTest, get_char_by_id
 
 app_name='core_api'
 
 urlpatterns = [
     path ('starwars_test', charTest),
+    path('character/<int:id>', get_char_by_id),
 ]
--- app/core_api/views.py
+++ app/core_api/views.py
@@ -3,11 +3,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.core import serializers
 
-def charTest(request):
-    if request.method == "GET": 
-        response = requests.get('https://swapi.dev/api/people/1/')
+    
+def get_char_by_id(request, id):
+    if request.method == "GET":
+        url =  "https://swapi.dev/api/people/"+str(id)+"/"
+        response = requests.get(url)
         char = response.json()
         return HttpResponse(response, content_type="application/json")
-    
       
 
